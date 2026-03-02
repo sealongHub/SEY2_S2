86456985c56ae5278993872a88a40ff0261be5cf
sealongHub <long638183@gmail.com>
2026-03-02 08:19:37 +0700

    Final fix: Corrected request logic and headers

--- web_pos/src/util/config.js
+++ web_pos/src/util/config.js
@@ -1,11 +1,5 @@
-// frontend/src/Config.js
 export default {
     base_url: import.meta.env.VITE_API_URL || 'https://sey2-s2.onrender.com/api/',
-    
-    // កែមកបែបនេះ (ដក api/ ចេញ)
+
     imgPath: import.meta.env.VITE_IMG_PATH || 'https://sey2-s2.onrender.com/storage/',
 }
-// export default {
-//     base_url : 'http://localhost:8000/api/',
-//     imgPath : 'http://localhost:80/SEY2_S2/laravel-api/public/storage/',
-// }
